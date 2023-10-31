2828c0377a6698e8c17fb31063e7ae25ce18573c
ИC201-2 <IS201-2@tisbi.ru>
2023-10-31 17:59:31 +0300

    first commit

---
+++ WpfApp1/WpfApp1/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1;
+using WpfApp1.Model;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            var user = App.DB.users.FirstOrDefault(x => x.login == UsernameTextBox.Text);
+
+            if(user == null || user.password != PasswordTextBox.Text)
+            {
+                MessageBox.Show("Данные для входа неверны");
+                return;
+            }
+
+            App.LoginUser = user;
+            NavigationService.Navigate(new Main());
+
+        
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/Pages/Main.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ComputersListButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ComputersPage());
+        }
+
+        private void ClassListButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ClassPage());
+        }
+    }
+}
